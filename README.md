369e30be971d84bc77f61a932f7cc263eccaeaee
Abdulrahman Atef <sa3d.clay@gmail.com>
2023-04-22 08:38:24 +0200

    update auth controller - delete some comments update README file - add jwt artisan command

--- README.md
+++ README.md
@@ -9,6 +9,7 @@
 ```
 composer install
 php artisan key:generate
+php artisan jwt:secret
 php artisan migrate
 php artisan serve
 ```
--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use JWTAuth;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 use App\Http\Requests\UserLoginRequest;
 use App\Http\Requests\UserRegisterRequest;
 use App\Http\Resources\User as UserResource;
@@ -12,8 +12,6 @@ use App\Http\Resources\User as UserResource;
 class AuthController extends Controller
 {
     public function register(UserRegisterRequest $req) {
-        // dd('you hit register');
-
         $role = 1;
         if(isset($req->role)) {
             $role = $req->role;
@@ -26,30 +24,12 @@ class AuthController extends Controller
             'role'      =>  $role,
             'planet'    =>  $req->planet,
         ]);
-
-        // create token method 1
         $token = JWTAuth::fromUser($user);
 
-        // create token method 2
-        // $credentials = $req->only(['email', 'password']);
-        // if (!$token = auth()->attempt($credentials)) {
-        //     return response()->json(['error' => 'Unauthorized'], 401);
-        // }
-
-        // response method 1
         return (new UserResource($user))
             ->additional(['meta' => [
                 'token' => $token,
             ]]);
-
-        // response method 2
-        // return response()->json(compact('user', 'token'), 201);
-        
-        // response method 3
-        // return response()->json([
-        //     'data' => $user,
-        //     'token' => $token,
-        // ], 201);
     }
 
     public function login(UserLoginRequest $req) {
@@ -58,12 +38,6 @@ class AuthController extends Controller
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
-        // return response()->json([
-        //     'data' => auth()->user(),
-        //     'token' => $token,
-        // ], 200);
-
-        // method 2
         return (new UserResource(auth()->user()))
             ->additional(['meta' => [
                 'token' => $token,
